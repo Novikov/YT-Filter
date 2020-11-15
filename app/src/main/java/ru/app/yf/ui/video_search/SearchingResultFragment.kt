f406bb856e5b0d8432221ad41c608d0380871769
Igor Novikov <novikov.mail@inbox.ru>
2020-11-15 15:04:23 +0300

    SearchResultFragment works

--- app/src/main/java/ru/app/yf/ui/video_search/SearchingResultFragment.kt
+++ app/src/main/java/ru/app/yf/ui/video_search/SearchingResultFragment.kt
@@ -2,14 +2,28 @@ package ru.app.yf.ui.video_search
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.TextView
+import androidx.databinding.Observable
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_searching_result.*
+import kotlinx.android.synthetic.main.fragment_searching_result.view.*
 import ru.app.yf.R
+import ru.app.yf.data.api.YouTubeClient
+import ru.app.yf.data.model.Video
+import ru.app.yf.data.repository.NetworkState
 import ru.app.yf.ui.IActivity
 
 class SearchingResultFragment : Fragment() {
@@ -37,6 +51,19 @@ class SearchingResultFragment : Fragment() {
         //Getting search request string from navController
         val args = SearchingResultFragmentArgs.fromBundle(requireArguments())
         searchRequest = args.SearchRequest
+
+        val apiClient = YouTubeClient.getClient()
+        searchVideosRepository = SearchVideosRepository(apiClient)
+
+        searchVideosViewModel = ViewModelProviders.of(this, object : ViewModelProvider.NewInstanceFactory() {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return SearchVideosViewModel(searchVideosRepository) as T
+            }
+        }).get(SearchVideosViewModel::class.java)
+
+        Log.e("NetworkStateOncreate",searchVideosViewModel.networkState.get()?.status.toString())
+
+        searchVideosViewModel.searchRequest(searchRequest)
     }
 
     override fun onCreateView(
@@ -50,7 +77,87 @@ class SearchingResultFragment : Fragment() {
         val editText = view.findViewById<SearchEditText>(R.id.searchEditText)
         editText.setText(searchRequest, TextView.BufferType.EDITABLE)
 
+
+
+        searchVideosViewModel.searchResultsLiveData.observe(viewLifecycleOwner, Observer {
+            updateRecyclerView(it)
+            searchEditText.clearFocus()
+        })
+
+        searchVideosViewModel.networkState.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback(){
+            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                if (searchVideosViewModel.networkState.get()!= NetworkState.WAITING){
+                    when(searchVideosViewModel.networkState.get()){
+                        NetworkState.LOADING -> {
+                            activityContract?.showProgressBar()
+                        }
+                        NetworkState.LOADED -> {
+                            activityContract?.hideProgressBar()
+                        }
+                        NetworkState.NO_INTERNET -> {
+                            activityContract?.hideProgressBar()
+                            activityContract?.showErrorDialog(NetworkState.NO_INTERNET.msg)
+                        }
+                        NetworkState.ERROR -> {
+                            activityContract?.hideProgressBar()
+                            activityContract?.showErrorDialog(NetworkState.ERROR.msg)
+                        }
+                        NetworkState.API_LIMIT_EXCEEDED -> {
+                            activityContract?.hideProgressBar()
+                            activityContract?.showErrorDialog(NetworkState.API_LIMIT_EXCEEDED.msg)
+                        }
+                    }
+                }
+            }
+        })
+
+        searchRecyclerView = view.search_results_rv1
+        searchRecyclerView.layoutManager = LinearLayoutManager(context)
+        adapter = SearchVideosAdapter(
+            mutableListOf()
+        )
+        searchRecyclerView.adapter = adapter
+        return view
+
         return view;
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activityContract?.showStatusBar()
+        navController = Navigation.findNavController(view)
+
+        //search button pressed
+        searchEditText.setOnEditorActionListener(TextView.OnEditorActionListener(function = { v, actionId, event ->
+            if (actionId== EditorInfo.IME_ACTION_SEARCH
+            ) {
+                val query = searchEditText.text.toString()
+                searchVideosViewModel.searchRequest(query)
+                true
+            }
+            else false
+        }))
+    }
+
+
+    fun updateRecyclerView(movieList: List<Video>) {
+        adapter.videos.clear()
+        adapter.videos.addAll(movieList)
+        adapter.notifyDataSetChanged()
+    }
+
+    fun clearRecyclerView(){
+        adapter.videos.clear()
+        adapter.notifyDataSetChanged()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        activityContract = null
     }
 
 }
