377cab2bc7ccce08a282e571d14f3296f394db94
Igor Novikov <novikov.mail@inbox.ru>
2021-01-04 18:54:07 +0300

    Preparing to release: 2/3

--- app/src/main/java/ru/app/yf/data/api/YouTubeClient.kt
+++ app/src/main/java/ru/app/yf/data/api/YouTubeClient.kt
@@ -16,7 +16,7 @@ import java.util.concurrent.TimeUnit
 object YouTubeClient {
         private const val YOUTUBE_BASE_URL="https://www.googleapis.com/youtube/v3/"
         lateinit var API_KEY:String
-        const val VIDEOS_PER_PAGE = "10"
+        const val VIDEOS_PER_PAGE = "30"
         const val URL_SNIPPET = "snippet"
         const val URL_STATISTICS = "statistics"
         const val URL_CONTENT_DETAILS = "contentDetails"
